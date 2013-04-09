30a8fde3e587f4b29ef9e774e336417d6f4aec4d
Patrick Browne <pt.browne@gmail.com>
2013-04-10 01:39:36 +0200

    initial commit

---
+++ server.py
@@ -0,0 +1,34 @@
+import codecs
+import sh
+import os
+import json
+from sh import pandoc, rm, convert
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/latex", methods=["POST"])
+def latex():
+    with codecs.open("temp.md", "w+", "utf-8") as f:
+        f.write(request.form["content"])
+    args = "temp.md --latex-engine=xelatex -t beamer -V theme:Carsurfing -o temp.pdf --smart"
+    args = "temp.md --latex-engine=xelatex -t beamer -o temp.pdf --smart"
+    pandoc_status = pandoc(*args.split(" "))
+
+    rm_status = rm(sh.glob("static/images/*"), "-r")
+
+    args = "-quality 100 -density 200x200 temp.pdf static/images/output%d.jpg"
+    convert_status = convert(*args.split(" "))
+
+    image_dir = "static/images"
+    files = os.listdir(image_dir)
+
+    print pandoc_status, rm_status, convert_status
+
+    return json.dumps(files)
+
+app.run(debug=True)
---
+++ temp.md
@@ -0,0 +1,28 @@
+EpicEditor + Markdown + Pandoc + Beamer + Convert + Flask
+=========
+
+C'est de la balle 
+
+---
+
+Salut Stelvio
+============
+
+Tu vas bien ?
+
+-----
+
+ouais !
+
+
+Ca va bien
+==========
+
+Je suis heureux d'être ici ce soir parmi vous !
+
+C'est vrai
+==========
+
+comme je suis content !!!
+
+
